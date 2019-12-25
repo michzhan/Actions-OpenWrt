28ae83cbbadf0f81ca9f7553576a94768b43034f
michzhan <michzhan@users.noreply.github.com>
2019-12-25 23:19:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.123/g' package/base-files/files/bin/config_generate
+
+# Modify max connection
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
